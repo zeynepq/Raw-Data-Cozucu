8bbd3300524bd0821264cc2cc4c46f397a6fedec
zeynepq <36823619+zeynepq@users.noreply.github.com>
2020-02-13 01:01:43 +0300

    Add files via upload

---
+++ RawDataCozucu/RawDataCozucu/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace RawDataCozucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEn = new System.Windows.Forms.TextBox();
+            this.txtBoy = new System.Windows.Forms.TextBox();
+            this.cmbFormat = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "En :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Boy : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Format : ";
+            // 
+            // txtEn
+            // 
+            this.txtEn.Location = new System.Drawing.Point(112, 57);
+            this.txtEn.Name = "txtEn";
+            this.txtEn.Size = new System.Drawing.Size(124, 20);
+            this.txtEn.TabIndex = 3;
+            // 
+            // txtBoy
+            // 
+            this.txtBoy.Location = new System.Drawing.Point(112, 102);
+            this.txtBoy.Name = "txtBoy";
+            this.txtBoy.Size = new System.Drawing.Size(124, 20);
+            this.txtBoy.TabIndex = 4;
+            // 
+            // cmbFormat
+            // 
+            this.cmbFormat.FormattingEnabled = true;
+            this.cmbFormat.Location = new System.Drawing.Point(112, 140);
+            this.cmbFormat.Name = "cmbFormat";
+            this.cmbFormat.Size = new System.Drawing.Size(121, 21);
+            this.cmbFormat.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(365, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tag = "timer1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbFormat);
+            this.Controls.Add(this.txtBoy);
+            this.Controls.Add(this.txtEn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEn;
+        private System.Windows.Forms.TextBox txtBoy;
+        private System.Windows.Forms.ComboBox cmbFormat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RawDataCozucu/RawDataCozucu/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RawDataCozucu
+{
+       public struct frame
+    {
+        public byte[] yDegerleri;
+        public byte[] uDegerleri;
+        public byte[] vDegerleri;
+    };
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbFormat.Items.Add("4:4:4");
+            cmbFormat.Items.Add("4:2:2");
+            cmbFormat.Items.Add("4:2:0");
+        }
+        
+        private byte[] Okuma(FileStream file)
+        {
+            int a = 0;
+            int sayac = 0;
+
+            byte[] icerik = new byte[file.Length];
+            
+            while (a>-1)
+            {
+                a = file.ReadByte();
+                if(a != -1 )
+                {
+                    icerik[sayac] = (byte)a;
+                }
+                sayac++;
+            }
+            file.Close();
+            return icerik;
+        }
+
+        private frame[] YuvBulma(byte[] dizi,int Boyut, string format)
+        {
+            int index = 0, counter = 1, yindex = 0,uindex = 0, vindex=0;
+            frame[] frameler = new frame[10000];
+            //frameler = new frame[dizi.Length / (int)(Boyut * 1.5)]; //4:2:0 için 
+
+            if (format == "4:2:0")
+            {
+                frameler = new frame[dizi.Length / (int)(Boyut * 1.5)]; //4:2:0 için 
+
+                for (int j = 0; j < frameler.Length; j++)
+                {
+                    frameler[j].yDegerleri = new byte[Boyut];
+                    frameler[j].vDegerleri = new byte[(int)(Boyut / 4)];
+                    frameler[j].uDegerleri = new byte[(int)(Boyut / 4)];
+                }
+
+               
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    if (counter == 1)
+                    {
+                        if (yindex != Boyut)
+                        {
+                            frameler[index].yDegerleri[yindex] = dizi[i];
+                            yindex++;
+                        }
+                        else
+                            counter = 2;
+                    }
+                    if (counter == 2)
+                    {
+                        if (uindex != (Boyut / 4))
+                        {
+                            frameler[index].uDegerleri[uindex] = dizi[i];
+                            uindex++;
+                        }
+                        else
+                            counter = 3;
+                    }
+                    if (counter == 3)
+                    {
+                        if (vindex != (Boyut / 4))
+                        {
+                            frameler[index].vDegerleri[vindex] = dizi[i];
+                            vindex++;
+                        }
+                        else
+                        {
+                            counter = 1;
+                            uindex = 0;
+                            vindex = 0;
+                            yindex = 0;
+                            index++;
+                            i--;
+                        }
+                    }
+                }
+            }
+            else if(format == "4:2:2")
+            {
+                frameler = new frame[dizi.Length / (int)(Boyut * 2)]; //4:2:2 için 
+
+                for (int j = 0; j < frameler.Length; j++)
+                {
+                    frameler[j].yDegerleri = new byte[Boyut];
+                    frameler[j].vDegerleri = new byte[(int)(Boyut / 2)];
+                    frameler[j].uDegerleri = new byte[(int)(Boyut / 2)];
+                }
+
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    if (counter == 1)
+                    {
+                        if (yindex != Boyut)
+                        {
+                            frameler[index].yDegerleri[yindex] = dizi[i];
+                            yindex++;
+                        }
+                        else
+                            counter = 2;
+                    }
+                    if (counter == 2)
+                    {
+                        if (uindex != (Boyut / 2))
+                        {
+                            frameler[index].uDegerleri[uindex] = dizi[i];
+                            uindex++;
+                        }
+                        else
+                            counter = 3;
+                    }
+                    if (counter == 3)
+                    {
+                        if (vindex != (Boyut / 2))
+                        {
+                            frameler[index].vDegerleri[vindex] = dizi[i];
+                            vindex++;
+                        }
+                        else
+                        {
+                            counter = 1;
+                            uindex = 0;
+                            vindex = 0;
+                            yindex = 0;
+                            index++;
+                            i--;
+                        }
+                    }
+                }
+
+            }
+            else if (format == "4:4:4")
+            {
+                frameler = new frame[dizi.Length / (int)(Boyut * 4)]; //4:4:4 için 
+
+                for (int j = 0; j < frameler.Length; j++)
+                {
+                    frameler[j].yDegerleri = new byte[Boyut];
+                    frameler[j].vDegerleri = new byte[(int)(Boyut)];
+                    frameler[j].uDegerleri = new byte[(int)(Boyut)];
+                }
+
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    if(index != frameler.Length)
+                    {
+                        if (yindex != Boyut)
+                        {
+                            frameler[index].yDegerleri[yindex] = dizi[i];
+                            frameler[index].uDegerleri[yindex] = dizi[i + Boyut];
+                            frameler[index].vDegerleri[yindex] = dizi[i + Boyut + Boyut];
+                            yindex++;
+                        }
+                        else
+                        {
+                            yindex = 0;
+                            index++;
+                        }
+                    }
+                    
+                    
+                    
+                }
+            }
+
+            return frameler;
+        }
+
+        private void YuvtoBitmap(frame[] frames,int en, int boy )
+        {
+            Bitmap bm = new Bitmap(en, boy);
+            int count = frames.Length;
+            for(int y=0; y<count; y++)
+            {
+                for (int i = 0; i < en; i++)
+                {
+                    for (int j = 0; j < boy; j++)
+                    {
+                        bm.SetPixel(i, j, Color.FromArgb(frames[y].yDegerleri[j * en + i], frames[y].yDegerleri[j * en + i], frames[y].yDegerleri[j * en + i]));
+                    }
+                }
+                bm.Save("frame" + (y + 1) + ".bmp");
+                //pictureBox1.Image = Image.FromFile("C:\\Users\\Yagmur\\Desktop\\RawDataCozucu\\RawDataCozucu\\bin\\Debug\\frame" + (y + 1) + ".bmp");
+                pictureBox1.Image = bm;
+                this.Update();
+            }
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream file;
+            byte[] Dizi;
+            frame[] frame;
+            string format ="";
+            int en = Convert.ToInt32(txtEn.Text.ToString());
+            int boy = Convert.ToInt32(txtBoy.Text.ToString());
+            int Boyut = en * boy;
+
+            if (cmbFormat.SelectedItem.ToString() == "4:2:0")
+            {
+                format = "4:2:0";
+                file = new FileStream("C:\\Users\\zynps\\OneDrive\\Masaüstü\\RawDataCozucu\\RawDataCozucu\\akiyo_qcif.yuv", FileMode.Open);
+                Dizi = Okuma(file);
+                frame = YuvBulma(Dizi, Boyut,format);
+                YuvtoBitmap(frame, en, boy);
+                
+            }
+            else if(cmbFormat.SelectedItem.ToString() == "4:2:2")
+            {
+                format = "4:2:2";
+                file = new FileStream("C:\\Users\\zynps\\OneDrive\\Masaüstü\\RawDataCozucu\\RawDataCozucu\\422P.yuv", FileMode.Open);
+                Dizi = Okuma(file);
+                frame = YuvBulma(Dizi, Boyut,format);
+               
+            }
+            else if(cmbFormat.SelectedItem.ToString() == "4:4:4")
+            {
+                format = "4:4:4";
+                file = new FileStream("C:\\Users\\zynps\\OneDrive\\Masaüstü\\RawDataCozucu\\RawDataCozucu\\444P.yuv", FileMode.Open);
+                Dizi = Okuma(file);
+                frame = YuvBulma(Dizi, Boyut,format);
+                YuvtoBitmap(frame, en, boy);
+            }
+
+        }
+        
+        
+    }
+}
